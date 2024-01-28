d4d290e18231326b17472f185130f12785170986
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2024-01-28 14:28:44 +0400

    hw2

---
+++ hw2/Singleton.java
@@ -0,0 +1,22 @@
+package hw2;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton1 == singleton2);
+    }
+}
